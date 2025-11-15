ea9bea57dea18b8178c041006a1ce5f08195df6d
Chloe Thompson <tchloev@gmail.com>
2025-11-15 13:43:21 -0500

    Org Login Updates and added some css

--- src/pages/org-login.jsx
+++ src/pages/org-login.jsx
@@ -1,38 +1,32 @@
-// import 'App.css'
+//import "tailwindcss";
+//import '../App.css';
 
 export default function OrgLogin() {
     return (
         <>
-        <h1>This is the login page</h1>
+        <div>
+        <h1 >networg</h1>
+
+      <div className="main-card">
+        <h2>
+          Enter Organization Code
+        </h2>
+        <input
+        />
+        <a
+          href="#"
+        >
+        <div></div>
+          Need Code?
+        </a>
+        <div></div>
+        <button
+        >
+          Enter
+        </button>
+      </div>
+    </div>
         </>
     );
 }
 
-
-
-// const OrgLogin = () => {
-//     return (
-//         <div>
-// <html lang="en">
-//   <meta charset="UTF-8" />
-//   <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-//   <title>Networg</title>
- 
-// <body>
-//   <div>
-//     <h1 class="title">networg</h1>
-//     <div class="card">
-//       <h2>Enter Organization Code</h2>
-//       <input type="text" />
-//       <a href="#" class="small-link">Need Code?</a>
-//       <button>Enter</button>
-//     </div>
-//   </div>
-// </body>
-// </html>
-
-//         </div>
-//     );
-// };
-
-// export default OrgLogin;

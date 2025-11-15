954c6a3440ee5790866725ae4b5997529d596905
Chloe Thompson <tchloev@gmail.com>
2025-11-15 12:24:38 -0500

    Start app.jsx and basic page navigation

--- src/pages/org-login.jsx
+++ src/pages/org-login.jsx
@@ -1,28 +1,38 @@
-import 'App.css'
+// import 'App.css'
 
-const OrgLogin = () => {
+export default function OrgLogin() {
     return (
-        <div>
-<html lang="en">
-  <meta charset="UTF-8" />
-  <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-  <title>Networg</title>
- 
-<body>
-  <div>
-    <h1 class="title">networg</h1>
-    <div class="card">
-      <h2>Enter Organization Code</h2>
-      <input type="text" />
-      <a href="#" class="small-link">Need Code?</a>
-      <button>Enter</button>
-    </div>
-  </div>
-</body>
-</html>
-
-        </div>
+        <>
+        <h1>This is the login page</h1>
+        </>
     );
-};
+}
 
-export default OrgLogin;
+
+
+// const OrgLogin = () => {
+//     return (
+//         <div>
+// <html lang="en">
+//   <meta charset="UTF-8" />
+//   <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+//   <title>Networg</title>
+ 
+// <body>
+//   <div>
+//     <h1 class="title">networg</h1>
+//     <div class="card">
+//       <h2>Enter Organization Code</h2>
+//       <input type="text" />
+//       <a href="#" class="small-link">Need Code?</a>
+//       <button>Enter</button>
+//     </div>
+//   </div>
+// </body>
+// </html>
+
+//         </div>
+//     );
+// };
+
+// export default OrgLogin;

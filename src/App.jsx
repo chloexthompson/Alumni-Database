04f6cdfe79b51fc039b33f3c13aead52ca75eb53
fchang1 <fchang1@terpmail.umd.edu>
2025-11-15 17:08:57 -0500

    myprofile edit

--- src/App.jsx
+++ src/App.jsx
@@ -3,9 +3,9 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 import './App.css';
 
 // import your pages
-import OrgLogin from "./pages/org-login.jsx";
+// import OrgLogin from "./pages/org-login.jsx";
 // import Home from "./pages/home.jsx";
-// import MyProfile from "./pages/my-profile.jsx";
+import MyProfile from "./pages/my-profile.jsx";
 // import SignIn from "./pages/sign-in.jsx";
 // import ViewProfile from "./pages/profile.jsx";
 // import CreateProfile from "./pages/create-profile.jsx";
@@ -16,7 +16,8 @@ function App() {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/" element={<OrgLogin />} />
+        <Route path="/my-profile" element={<MyProfile />}/>
+        {/* <Route path="/" element={<OrgLogin />} /> */}
         {/* <Route path="/sign-in" element={<SignIn />}/>
         <Route path="/home" element={<Home />}/>
         <Route path="/my-profile" element={<MyProfile />}/>

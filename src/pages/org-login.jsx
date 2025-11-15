fb3bd7f7e111e3e3f129865ad8a633827a9f2017
Chloe Thompson <tchloev@gmail.com>
2025-11-15 11:50:16 -0500

    Started org login

---
+++ src/pages/org-login.jsx
@@ -0,0 +1,28 @@
+import 'App.css'
+
+const OrgLogin = () => {
+    return (
+        <div>
+<html lang="en">
+  <meta charset="UTF-8" />
+  <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+  <title>Networg</title>
+ 
+<body>
+  <div>
+    <h1 class="title">networg</h1>
+    <div class="card">
+      <h2>Enter Organization Code</h2>
+      <input type="text" />
+      <a href="#" class="small-link">Need Code?</a>
+      <button>Enter</button>
+    </div>
+  </div>
+</body>
+</html>
+
+        </div>
+    );
+};
+
+export default OrgLogin;

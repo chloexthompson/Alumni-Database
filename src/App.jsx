0c095c72bdb91285dce21a85d73eb30559dc741c
Chloe Thompson <tchloev@gmail.com>
2025-11-15 17:49:17 -0500

    Update app.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -4,15 +4,15 @@ import './App.css';
 import OrgLogin from "./pages/org-login.jsx";
  import Home from "./pages/home.jsx";
 // import MyProfile from "./pages/my-profile.jsx";
-// import SignIn from "./pages/sign-in.jsx";
-// import CreateProfile from "./pages/create-profile.jsx";
+ import SignIn from "./pages/sign-in.jsx";
+// import ViewProfile from "./pages/profile.jsx";
+import CreateProfile from "./pages/create-profile.jsx";
 // import AlumniProfile from "./pages/AlumniProfile";
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
-        {/* <Route path="/" element={<Profile />} /> */}
         <Route path="/" element={<OrgLogin />} />
         <Route path="/sign-in" element={<SignIn />}/>
         <Route path="/home" element={<Home />}/>

8bfcad7e33f4c986046b9c17d41a50e63e8ba51c
poojatgit <ptummala@terpmail.umd.edu>
2025-11-15 21:36:17 -0500

    added profile.css + updated app.jsx + profile.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,10 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import './App.css';
+import "./pages/profile.css";
 
 // import your pages
-// import Profile from "./pages/profile.jsx";
-import OrgLogin from "./pages/org-login.jsx";
+import Profile from "./pages/profile.jsx";
+// import OrgLogin from "./pages/org-login.jsx";
 // import Home from "./pages/home.jsx";
 // import MyProfile from "./pages/my-profile.jsx";
 // import SignIn from "./pages/sign-in.jsx";
@@ -12,11 +13,21 @@ import OrgLogin from "./pages/org-login.jsx";
 // import About from "./pages/About";
 
 function App() {
+  const userData = {
+    firstName: "Alex",
+    lastName: "Wang",
+    major: "Finance",
+    gradYear: 2027,
+    referralStatus: "Pending",
+    description: "This is a short profile description about Alex Wang.",
+    skills: ["Mathamatics", "Finance Bro", "Investing"]
+  };
   return (
     <BrowserRouter>
       <Routes>
+        <Route path="/" element={<Profile {...userData} />} />
         {/* <Route path="/" element={<Profile />} /> */}
-        <Route path="/" element={<OrgLogin />} />
+        {/* <Route path="/" element={<OrgLogin />} /> */}
         {/* <Route path="/sign-in" element={<SignIn />}/>
         <Route path="/home" element={<Home />}/>
         <Route path="/" element={<OrgLogin />} />

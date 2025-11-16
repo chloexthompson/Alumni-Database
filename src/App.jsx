7c12534549415c5bc60efe10e552db5f2ce87f09
fchang1 <fchang1@terpmail.umd.edu>
2025-11-15 20:54:48 -0500

    email edit

--- src/App.jsx
+++ src/App.jsx
@@ -4,12 +4,12 @@ import './App.css';
 // import your pages
 
 
-import OrgLogin from "./pages/org-login.jsx";
-import Home from "./pages/home.jsx";
-// import MyProfile from "./pages/my-profile.jsx";
-import SignIn from "./pages/sign-in.jsx";
+// import OrgLogin from "./pages/org-login.jsx";
+// import Home from "./pages/home.jsx";
+import MyProfile from "./pages/my-profile.jsx";
+// import SignIn from "./pages/sign-in.jsx";
 // import ViewProfile from "./pages/profile.jsx";
-import CreateProfile from "./pages/create-profile.jsx";
+// import CreateProfile from "./pages/create-profile.jsx";
 // import AlumniProfile from "./pages/AlumniProfile";
 // import About from "./pages/About";
 
@@ -17,7 +17,7 @@ function App() {
   return (
     <BrowserRouter>
       <Routes>
-
+        <Route path="/my-profile" element={<MyProfile />}/>
         {/* <Route path="/" element={<OrgLogin />} />
         <Route path="/sign-in" element={<SignIn />}/> */}
         {/*<Route path="/home" element={<Home />}/>
@@ -29,12 +29,12 @@ function App() {
         <Route path="/view-profile" element={<ViewProfile />}/>
         <Route path="/create-profile" element={<CreateProfile />}/> */}
       
-        <Route path="/create-profile" element={<CreateProfile />}/>
-=======
+        {/* <Route path="/create-profile" element={<CreateProfile />}/>
+
         <Route path="/" element={<OrgLogin />} />
         <Route path="/sign-in" element={<SignIn />}/>
         <Route path="/home" element={<Home />}/>
-        <Route path="/create-profile" element={<CreateProfile />}/>
+        <Route path="/create-profile" element={<CreateProfile />}/> */}
         {/*<Route path="/my-profile" element={<MyProfile />}/>
         <Route path="/view-profile" element={<ViewProfile />}/>
          */}

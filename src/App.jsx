19181c10bb1b1784af09cbb1bd47d9ed088df3ce
eldaahz <ezelalem23@gmail.com>
2025-11-15 20:51:12 -0500

    app.jsx edit

--- src/App.jsx
+++ src/App.jsx
@@ -2,35 +2,28 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 import './App.css';
 
 // import your pages
-<<<<<<< HEAD
 // import OrgLogin from "./pages/org-login.jsx";
 import OrgLogin from "./pages/org-login.jsx";
 // import Home from "./pages/home.jsx";
 // import MyProfile from "./pages/my-profile.jsx";
  import SignIn from "./pages/sign-in.jsx";
-=======
 
 
 import OrgLogin from "./pages/org-login.jsx";
 import Home from "./pages/home.jsx";
 // import MyProfile from "./pages/my-profile.jsx";
 import SignIn from "./pages/sign-in.jsx";
->>>>>>> 3dce0ac958c94c1328f493b2b5ecf17b678c65df
+
 // import ViewProfile from "./pages/profile.jsx";
 import CreateProfile from "./pages/create-profile.jsx";
 // import AlumniProfile from "./pages/AlumniProfile";
 // import About from "./pages/About";
-<<<<<<< HEAD
 // import CreateProfile from "./pages/create-profile.jsx";
-=======
->>>>>>> 3dce0ac958c94c1328f493b2b5ecf17b678c65df
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
-<<<<<<< HEAD
-=======
 
         {/* <Route path="/" element={<OrgLogin />} />
         <Route path="/sign-in" element={<SignIn />}/> */}
@@ -44,20 +37,16 @@ function App() {
         <Route path="/create-profile" element={<CreateProfile />}/> */}
       
         <Route path="/create-profile" element={<CreateProfile />}/>
-=======
->>>>>>> 3dce0ac958c94c1328f493b2b5ecf17b678c65df
         <Route path="/" element={<OrgLogin />} />
         <Route path="/sign-in" element={<SignIn />}/>
         <Route path="/create-profile" element={<CreateProfile />}/>
-<<<<<<< HEAD
         {/*<Route path="/home" element={<Home />}/>
         <Route path="/my-profile" element={<MyProfile />}/>
         <Route path="/view-profile" element={<ViewProfile />}/> */}
-=======
+
         {/*<Route path="/my-profile" element={<MyProfile />}/>
         <Route path="/view-profile" element={<ViewProfile />}/>
          */}
->>>>>>> 3dce0ac958c94c1328f493b2b5ecf17b678c65df
       </Routes>
     </BrowserRouter>
   );

378b0ef13fff4f815ad9adfdbd56dde007ffd799
poojatgit <ptummala@terpmail.umd.edu>
2025-11-16 10:48:53 -0500

    header icon component

---
+++ src/components/header-icon.jsx
@@ -0,0 +1,11 @@
+export default function Header(){
+    return(
+        <div className="header-icons">
+                <div className="icon-container">
+                    <img src="https://cdn-icons-png.flaticon.com/128/1286/1286853.png" alt="logout" />
+                    <img src="https://cdn-icons-png.flaticon.com/128/15753/15753894.png" alt="profile" />
+                    <img src="https://cdn-icons-png.flaticon.com/128/1946/1946488.png" alt="home" />
+                </div>
+            </div>
+    )
+}

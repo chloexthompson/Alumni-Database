17f8a39722e9b6548f842563246b5bd17bee763c
poojatgit <ptummala@terpmail.umd.edu>
2025-11-16 20:36:26 -0500

    random

--- src/components/profile-card.jsx
+++ src/components/profile-card.jsx
@@ -1,33 +1,3 @@
-// import styles from "./profile-card.module.css";
-
-// const showGradStatus = (gradStatus) => {
-//   return gradStatus ? "Alumni" : "Student";
-// };
-
-// const ProfileCard = ({ name, gradStatus, gradYear }) => {
-//   return (
-   
-//     <div id={styles.card} >
-//       <img src="src/assets/profile.png" className={styles.headshotShape}  />
-
-//       <h2 className={styles.name}>{name}</h2>
-
-//       <h3 className={styles.status}>{showGradStatus(gradStatus)}</h3>
-
-//       <div className={styles.gradYearBox}>
-//         Class of {gradYear}
-//       </div>
-
-//       <a href="/view-profile">
-//       <button>View Profile</button>
-//       </a>
-//     </div>
-   
-//   );
-// };
-
-// export default ProfileCard;
-
 import { useNavigate } from "react-router-dom";
 import styles from "./profile-card.module.css";
 
